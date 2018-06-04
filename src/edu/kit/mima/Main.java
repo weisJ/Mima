729fdb954eeaef93d191fe0e48bccba62379d4f4
weisj <weisj@arcor.de>
2018-06-04 11:40:02 +0200

    changed readme

--- src/edu/kit/mima/Main.java
+++ src/edu/kit/mima/Main.java
@@ -46,22 +46,7 @@ public final class Main extends JFrame {
         editor = new Editor();
         console = new Console();
         memoryView = new MemoryView(new String[]{"Address", "Value"});
-        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                try {
-                    if (unsaved) savePopUp();
-                    optionsLoader.saveOptions(lastFile);
-                    e.getWindow().dispose();
-                } catch (IOException | IllegalArgumentException ignored) { }
-            }
-        });
-        setResizable(true);
-        setSize((int) FULLSCREEN.getWidth() / 2, (int) FULLSCREEN.getHeight() / 2);
-        setTitle(TITLE);
-        setLayout(new BorderLayout());
-        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("mima.png")));
+        setupWindow();
 
         JPanel memoryConsole = new JPanel(new GridLayout(2, 1));
         memoryConsole.add(memoryView);
@@ -69,26 +54,7 @@ public final class Main extends JFrame {
         add(memoryConsole, BorderLayout.LINE_START);
 
         setupButtons();
-        Menu menu = new Menu();
-        JMenu file = new JMenu("File");
-        JMenuItem newFile = new JMenuItem("New");
-        newFile.addActionListener(e -> newFile());
-        file.add(newFile);
-        file.addSeparator();
-        JMenuItem save = new JMenuItem("Save");
-        save.addActionListener(e -> {
-            if (unsaved) {
-                saveAs();
-            } else {
-                save();
-            }
-        });
-        file.add(save);
-        JMenuItem saveAs = new JMenuItem("Save as");
-        saveAs.addActionListener(e -> saveAs());
-        file.add(saveAs);
-        menu.add(file);
-        setJMenuBar(menu);
+        setupMenu();
         add(editor, BorderLayout.CENTER);
 
         LoadManager loadManager = new DefaultLoadManager() {
@@ -130,19 +96,108 @@ public final class Main extends JFrame {
             String text = saveHandler.loadFile(lastFile);
             editor.setText(text);
             lines = text.split("\n");
+            mimaXFile = lastFile.endsWith(FILE_EXTENSION_X);
         } catch (IOException e) {
             firstStart();
         }
-        setSyntaxHighlighting();
+        updateSyntaxHighlighting();
         editor.setStylize(true);
         editor.stylize();
     }
-
 
     public static void main(String[] args) {
         Main frame = new Main();
         frame.setVisible(true);
         frame.repaint();
+    }
+
+    private void setupWindow() {
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                try {
+                    if (unsaved) savePopUp();
+                    optionsLoader.saveOptions(lastFile);
+                    e.getWindow().dispose();
+                } catch (IOException | IllegalArgumentException ignored) { }
+            }
+        });
+        setResizable(true);
+        setSize((int) FULLSCREEN.getWidth() / 2, (int) FULLSCREEN.getHeight() / 2);
+        setTitle(TITLE);
+        setLayout(new BorderLayout());
+        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("mima.png")));
+    }
+
+    private void setupMenu() {
+        Menu menu = new Menu();
+        JMenu file = new JMenu("File");
+        JMenuItem newFile = new JMenuItem("New");
+        newFile.addActionListener(e -> newFile());
+        newFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+        file.add(newFile);
+        JMenuItem load = new JMenuItem("Load");
+        load.addActionListener(e -> load(textLoader.requestLoad(directory,
+                                                                new String[]{FILE_EXTENSION, FILE_EXTENSION_X},
+                                                                () -> { })));
+        load.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_DOWN_MASK));
+        file.add(load);
+
+        file.addSeparator();
+
+        JMenuItem save = new JMenuItem("Save");
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+        save.addActionListener(e -> {
+            if (unsaved) saveAs();
+            else save();
+        });
+        file.add(save);
+        JMenuItem saveAs = new JMenuItem("Save as");
+        saveAs.addActionListener(e -> saveAs());
+        saveAs.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,
+                                                     InputEvent.CTRL_DOWN_MASK | InputEvent.ALT_DOWN_MASK));
+        file.add(saveAs);
+        menu.add(file);
+        setJMenuBar(menu);
+    }
+
+    private void setupButtons() {
+        shortcutButton(run, KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.CTRL_DOWN_MASK), this::run);
+        shortcutButton(step,
+                       KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK | InputEvent.ALT_DOWN_MASK),
+                       this::step);
+        JButton compile = new JButton("COMPILE");
+        shortcutButton(compile, KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK), this::compile);
+        JButton reset = new JButton("RESET");
+        shortcutButton(reset,
+                       KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.CTRL_DOWN_MASK | InputEvent.ALT_DOWN_MASK),
+                       this::compile);
+
+        run.setEnabled(false);
+        step.setEnabled(false);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(1, 3));
+        panel.add(compile);
+        panel.add(reset);
+        panel.add(step);
+        panel.add(run);
+        add(panel, BorderLayout.PAGE_START);
+    }
+
+    private void shortcutButton(JButton button, KeyStroke keyStroke, Runnable action) {
+        Action buttonAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Action performed");
+                action.run();
+            }
+        };
+        String name = button.getName();
+        this.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(keyStroke, name);
+        this.getRootPane().getActionMap().put(name, buttonAction);
+        button.addActionListener(e -> action.run());
     }
 
     private void firstStart() {
@@ -164,16 +219,19 @@ public final class Main extends JFrame {
         }
     }
 
-    private void newFile() {
-        int response = JOptionPane.showOptionDialog(this, "Choose file type", "File type",
-                                                    JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
-                                                    null, new String[]{"Mima", "MimaX"}, "Mima");
-        if (response == JOptionPane.CLOSED_OPTION) return;
-        mimaXFile = response == 1;
-        setSyntaxHighlighting();
-        lines = new String[]{"#Put Code here"};
-        editor.setText("#Put Code here");
-        unsaved = true;
+    private void step() {
+        try {
+            log("Step!");
+            mima.step();
+            run.setEnabled(false);
+            log("Instruction: " + lines[mima.getCurrentLineIndex() - 1]);
+            updateMemoryTable();
+            step.setEnabled(mima.isRunning());
+        } catch (IllegalArgumentException e) {
+            error(e.getMessage());
+            step.setEnabled(false);
+            run.setEnabled(false);
+        }
     }
 
     private void run() {
@@ -182,14 +240,13 @@ public final class Main extends JFrame {
         log("Running program: " + lastFile + "...");
         try {
             mima.run();
-            update();
+            updateMemoryTable();
         } catch (IllegalArgumentException e) {
             error(e.getMessage());
             reset();
-        } finally {
-            step.setEnabled(true);
-            run.setEnabled(true);
         }
+        step.setEnabled(true);
+        run.setEnabled(true);
         log("done");
     }
 
@@ -197,81 +254,39 @@ public final class Main extends JFrame {
         mima.reset();
         run.setEnabled(true);
         step.setEnabled(true);
-        update();
+        updateMemoryTable();
     }
 
-    private void step() {
-        try {
-            log("Step!");
-            mima.step();
-            run.setEnabled(false);
-            log("Instruction: " + lines[mima.getCurrentLineIndex() - 1]);
-            update();
-            step.setEnabled(mima.isRunning());
-        } catch (IllegalArgumentException e) {
-            error(e.getMessage());
-        }
+
+    private void newFile() {
+        int response = JOptionPane.showOptionDialog(this, "Choose file type", "File type",
+                                                    JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                                                    null, new String[]{"Mima", "MimaX"}, "Mima");
+        if (response == JOptionPane.CLOSED_OPTION) return;
+        mimaXFile = response == 1;
+        updateSyntaxHighlighting();
+        lines = new String[]{"#Put Code here"};
+        editor.setText("#Put Code here");
+        unsaved = true;
     }
 
     private void load(String text) {
         String corrected = text.replaceAll("\r?\n", "\n");
         editor.setText(corrected);
         lines = corrected.split("\n");
-        setSyntaxHighlighting();
+        updateSyntaxHighlighting();
         editor.stylize();
         unsaved = false;
     }
 
-    private void update() {
-        memoryView.setContent(mima.memoryTable());
-        repaint();
-    }
-
-    private void setupButtons() {
-        run.addActionListener(e -> run());
-        step.addActionListener(e -> step());
-        JButton load = new JButton("LOAD");
-        load.addActionListener(e -> load(textLoader.requestLoad(directory, new String[]{FILE_EXTENSION, FILE_EXTENSION_X}, () -> { })));
-        JButton compile = new JButton("COMPILE");
-        compile.addActionListener(e -> compile());
-        JButton reset = new JButton("RESET");
-        reset.addActionListener(e -> reset());
-
-        run.setEnabled(false);
-        step.setEnabled(false);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 4));
-        panel.add(load);
-        panel.add(compile);
-        panel.add(reset);
-        panel.add(step);
-        panel.add(run);
-        add(panel, BorderLayout.PAGE_START);
-    }
-
-    private void setSyntaxHighlighting() {
-        Color instructionsColor = new Color(27, 115, 207);
-        Color keywordColor = new Color(168, 120, 43);
-        editor.setHighlight(Mima.getInstructionSet(), instructionsColor);
-        editor.addHighlight(Interpreter.getKeywords(), new Color[]{
-                keywordColor, //$define
-                keywordColor, // :
-                keywordColor, // (
-                keywordColor, // )
-                new Color(37, 143, 148), //Numbers
-                new Color(136, 64, 170), //0b,
-                new Color(63, 135, 54), //Comments
-        });
-        if (mimaXFile) {
-            editor.addHighlight(Mima.getMimaXInstructionSet(), instructionsColor);
+    private void reloadMima() {
+        try {
+            mima.loadProgram(lines.clone(), mimaXFile);
+            run.setEnabled(false);
+            step.setEnabled(false);
+        } catch (IllegalArgumentException e) {
+            error(e.getMessage());
         }
-    }
-
-    private void saveAs() {
-        String extension = mimaXFile ? FILE_EXTENSION_X : FILE_EXTENSION;
-        textLoader.requestSave(editor.getText(), directory, extension, () -> { throw new IllegalArgumentException(); });
-        unsaved = false;
     }
 
     private void save() {
@@ -282,6 +297,15 @@ public final class Main extends JFrame {
         } catch (IOException e) {
             error("failed to save: " + e.getMessage());
         }
+    }
+
+    private void saveAs() {
+        String extension = mimaXFile ? FILE_EXTENSION_X : FILE_EXTENSION;
+        try {
+            textLoader.requestSave(editor.getText(), directory, extension,
+                                   () -> { throw new IllegalArgumentException(); });
+        } catch (IllegalArgumentException ignored) { }
+        unsaved = false;
     }
 
     private void savePopUp() {
@@ -303,11 +327,36 @@ public final class Main extends JFrame {
             reloadMima();
             run.setEnabled(true);
             step.setEnabled(true);
-            update();
+            updateMemoryTable();
         } catch (IllegalArgumentException e) {
             error(e.getMessage());
+            run.setEnabled(false);
+            step.setEnabled(false);
         }
         log("done");
+    }
+
+    private void updateMemoryTable() {
+        memoryView.setContent(mima.memoryTable());
+        repaint();
+    }
+
+    private void updateSyntaxHighlighting() {
+        Color instructionsColor = new Color(27, 115, 207);
+        Color keywordColor = new Color(168, 120, 43);
+        editor.setHighlight(Mima.getInstructionSet(), instructionsColor);
+        editor.addHighlight(Interpreter.getKeywords(), new Color[]{
+                keywordColor, //$define
+                keywordColor, // :
+                keywordColor, // (
+                keywordColor, // )
+                new Color(37, 143, 148), //Numbers
+                new Color(136, 64, 170), //0b,
+                new Color(63, 135, 54), //Comments
+        });
+        if (mimaXFile) {
+            editor.addHighlight(Mima.getMimaXInstructionSet(), instructionsColor);
+        }
     }
 
     private void log(String message) {
@@ -320,13 +369,4 @@ public final class Main extends JFrame {
         repaint();
     }
 
-    private void reloadMima() {
-        try {
-            mima.loadProgram(lines.clone(), mimaXFile);
-            run.setEnabled(false);
-            step.setEnabled(false);
-        } catch (IllegalArgumentException e) {
-            error(e.getMessage());
-        }
-    }
 }
